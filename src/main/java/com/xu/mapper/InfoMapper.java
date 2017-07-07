a39d8e76cd50a34b8d6abe78e8be207542ba50d4
myherux <heroxu123@gmail.com>
2017-07-07 17:52:53 +0800

    feat: init

---
+++ src/main/java/com/xu/mapper/InfoMapper.java
@@ -0,0 +1,17 @@
+package com.xu.mapper;
+
+import com.xu.entity.Info;
+
+public interface InfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Info record);
+
+    int insertSelective(Info record);
+
+    Info selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Info record);
+
+    int updateByPrimaryKey(Info record);
+}
